815bb14ed899e3f306837d28f26e92724b7d4522
prakashsawle-codenser <52690773+prakashsawle-codenser@users.noreply.github.com>
2019-07-12 20:18:36 +0530

    Add files via upload

---
+++ sample.php
@@ -0,0 +1,23 @@
+<?php 
+ echo $user_id = $argv[1];  
+
+//echo  $result = file_get_contents("http://localhost/sokrets/updateAnswerApi/".$user_id);
+//  $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, "http://localhost/sokrets/test_url");
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// //$body = '{}';
+// //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
+// //curl_setopt($ch, CURLOPT_POSTFIELDS,$body);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// $authToken = curl_exec($ch);
+// echo $authToken;
+// if (curl_errno ($ch)) {   
+//             echo curl_error ( $ch );
+//             curl_close ( $ch );
+//             exit ();
+//         }
+// curl_close($ch);
+echo "f dsfsdfsdf";
+?>
